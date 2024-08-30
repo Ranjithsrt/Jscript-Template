dfe3e48d507ac98543013fab7dd2bc13cb82792d
Ranjithsrt <ranjith201099@gmail.com>
2024-08-30 20:36:26 +0530

    js-operators

--- L03 - Operators/index.js
+++ L03 - Operators/index.js
@@ -18,7 +18,7 @@ Types:
 
 console.log( "Arithmetic operators");
 
-let sum = 5 + 3;
+let sum = 5 + 3; //8
 
 console.log( "Addittion" , sum);
 
@@ -27,7 +27,7 @@ console.log( "Addittion" , sum);
   
 1.2)Subtraction (-)
  
-let sum = 5 - 3;
+let sum = 10 - 2;  //8
 
 console.log( "subtraction" , sum);
 
@@ -35,7 +35,7 @@ console.log( "subtraction" , sum);
 
 1.3)Multiplication (*)
 
-let sum = 2 * 3;
+let sum = 2 * 3; //6
 
 console.log( "mul" , sum);
 
@@ -43,7 +43,7 @@ console.log( "mul" , sum);
 
 1.4)Division (/)
 
- let sum = 12 / 2;
+ let sum = 12 / 2; //6
 
 console.log( "div" , sum);   
 
@@ -53,7 +53,7 @@ console.log( "div" , sum);
 
 let sum = 10 % 3;  //reminder
 
-console.log( "m0du" , sum);
+console.log( "modu" , sum);
 
 -----------------------------------------------------------------------------------------------
 
@@ -119,7 +119,7 @@ console.log( num);
 2.2)Addition Assignment (+=)
 
 let num =10;
-num+=5;
+num+=5; // num=10+5
 
 console.log( num);  //15
 
@@ -129,9 +129,9 @@ console.log( num);  //15
 
 
 let num =10;
-num-=5;
+num-=5; //num=10-5
 
-console.log( num);  //10
+console.log( num);  //5
 
 ----------------------------------------------------------------------------------------------------------
 
@@ -139,7 +139,7 @@ console.log( num);  //10
 
 
 let num =10;
-num*=5'
+num*=5;
 
 console.log( num);  //50
 
@@ -157,7 +157,7 @@ console.log( num);  //5
 2.6)Modulus Assignment (%=):
 
 let num =10;
-num%=3
+num%=3;
 
 console.log( num);  //1
 
@@ -180,7 +180,7 @@ console.log( 5==5);  //true
 
 Type coercion:
 
-console.log( 5==:"5");  //1st enna type paakum apram aathuve convert pnnikkum  //true
+console.log( 5=="5");  //1st enna type paakum apram aathuve convert pnnikkum  //true
 
 3.2)Strict Equal (===)
 
@@ -190,9 +190,9 @@ console.log( 5==='5');  // irendu ore value  vaa ore type ah check pannum?.. // 
 
 3.4)Not Equal (!=)
 
-console.log( 5!='5');  // irendum same aga orukka koodaathu.. trur
+console.log( 5!='5');  // irendum same aga orukka koodaathu.. true
 
-console.log( 4!=5);  //false
+console.log( 4!=5);  //true
 
 3.5)Strict Not Equal (!==)
 
@@ -219,7 +219,7 @@ console.log( 7>=10);  //false
 
 3.9)Less Than or Equal (<=)
 
- console.log( 5<=10);  //true
+console.log( 5<=10);  //true
 
 console.log( 10<=10);  //true
 
@@ -256,7 +256,7 @@ console.log(!false );  //t
 
 let haveidproof=false;
 let isAdult=true;
-
+       False         true
 if(haveidproof &&  isAdult){
 
     console.log( "allowed" );   
@@ -267,45 +267,56 @@ else{
   
 }
 
-
+Output : not allowed 
 -------------------------------------------------------------------------------------------------
 
- String Operators
+String Operators
 
 String concatenation:
 
-console.log( "HI");
+console.log( "Hello World.! ");
 
 console.log( "HI" +' '+" HELLO");  
 
 String with Different Quotes:
 
-console.log( " I AM 'HERO'");  
+console.log( " I AM 'HERO' ");  
 
-console.log( ' I AM "HERO"');  
-
-let MSG="I" + "RANJITH";
-console.log( MSG);  
+console.log( ' I AM "HERO" ');  
 
 console.log( " I \'M 'HERO' "); 
 
 Concatenation with Object Properties:
 
 let person={
-
 id:1,
-name:"ranjith",
-  
+name:"ranjith",  
 }
 
 console.log( " wlcome"+ name+ "yourid " + id);  
 
+________________________________________________________
+
 Template literal
 
-console.log( " wlcome ${ name} yourid ${id});  
+console.log ("welcome ${name} your ID ${id} " ) ;
 
-//Order Prcedence
+Order Prcedence:
 
-// New Examples with - and /
+console.log( 2 + 4 * 3 );  //4 * 3 = 12 + 2 = 14
 
-// Additional Combined Example
+console.log ((2 + 4) + 3);  // 2 + 4 = 6  * 3 = 18
+
+______________________________________________________________
+
+New Examples with and
+
+console.log (10 - 2/2) ;   // 2/2 = 1, then 10 - 1 = 9
+console.log((10-2) / 2);   // 10-2=8, then 8/2  = 4
+
+________________________________________________________________
+
+Additional Combined Example
+
+console.log((8/2)(2 + 2));   //2+2+4, 8/2 = 4, then 4* 4 =16
+ 
