f600f3750bcefc84455b0c01854e85719c87cfa4
Ranjithsrt <ranjith201099@gmail.com>
2024-09-02 22:04:16 +0530

    js -objects

--- L11 - Objects/index.js
+++ L11 - Objects/index.js
@@ -1,17 +1,145 @@
-// Adding a new property
 
-// Modifying an existing property
+let person = {
 
-// Object with method
+  key     value
+ name : "Ranjith",
+ age : 21,
+ isEmployed: true, 
+  
+};
 
-// Destructuring assignment
+console.log(person);
+console.log(person.name);
+console.log(person.age);
 
-//Nested Complex Objects
+--------------------------------------------------------------------------------------------------
+  
+Adding a new property
 
-// Accessing properties of the nested objects
+person.city ="newyork";  //add
+console.log(person);
 
-// Destructure owner object
+----------------------------------------------------------------------------------------------------
+  
+Modifying an existing property
 
-// Output details about the restaurant
+person.age =30;  //update
+console.log(person);
 
-// Output the menu items using destructuring within forEach
+
+-------------------------------------------------------------------------------------------------------]]
+
+Object with method
+
+let car ={
+
+  brand:"toyota";
+  model:"camry;
+  year=2026;
+  displayinfo:function(){
+               //(this) obj kuulla irukka year eaduthukko
+    return '${this.year} ${this.brand} ${this.model};
+  }
+
+};
+
+//console.log(car);
+console.log(car.displayinf());
+
+output:
+
+2026 toyota  camry
+ // brand:"toyota";
+  //model:"camry;
+  //year=2026;
+
+----------------------------------------------------------------------------------------------------------
+
+Destructuring assignment
+
+let person = {
+
+  key     value
+ name : "Ranjith",
+ age : 21,
+ isEmployed: true, 
+  
+};
+
+shorthand: (desturucturing)
+
+let {name ,age,isemployed} = person;   //mela irukka obj key value inga set agum
+
+console.log(name);
+console.log(age);
+
+
+Nested Complex Objects
+
+let resturent ={
+
+  name:"idli",
+  loc : "chennai";
+
+
+  owner :{
+             name : "Ranjith",
+              age : 21,
+
+
+                contact:{
+                   email: viratzzz@12.
+                   ph:"27839699",
+  };
+             };
+
+      menu:[
+
+
+          { dish: "masala dosa",price:30,spicy:true},
+          { dish: "coffee",price:100,spicy:false},
+          { dish: "pongal",price:45:,spicy:false},
+
+
+           ];
+  
+};
+
+console.log(restaurant);
+
+-----------------------------------------------------------------------------------------------------------------
+
+
+Accessing properties of the nested objects
+
+console.log("welcome to ${restaurant.name} in ${restaurant.loc}");
+console.log("welcome to ${restaurant.owner.name} in ${restaurant.owner.loc}");
+console.log("welcome to ${restaurant.owner.contact.email in ${restaurant.contact.ph}");
+
+restaurant.menu,forEach((item) =>{
+  
+  console.log(" ${iteam.dish}: rs. ${item.price}") (${item.spicy? "spicy":"not"});  // not spicy
+
+
+});
+
+---------------------------------------------------------------------------------------------------------------------------------
+
+Destructure owner object
+
+let { name:ownername,age:ownerage,contact:{ email: owneremail,phone: ownerphone} ,} =restaurant.owner;
+
+ Output details about the restaurant
+
+console.log("owned by  ${ownername} ,age ${ownerage}");
+console.log(" contact ${ownereamul} in ${ownerphone}");
+
+
+ Output the menu items using destructuring within forEach
+
+restaurant.menu,forEach(( dish,price,spicy) =>{
+  
+  console.log(" ${dish}: rs. ${price}") (${spicy? "spicy":"not"});  // not spicy
+});
+
+-----------------------------------------------------------------------------------------------------------------------------
