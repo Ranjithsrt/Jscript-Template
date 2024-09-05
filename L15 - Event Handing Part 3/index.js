4ac04a19b66381b196e2c2a96e1c5591044276ba
Ranjithsrt <ranjith201099@gmail.com>
2024-09-05 19:27:01 +0530

    js - Event H3

--- L15 - Event Handing Part 3/index.js
+++ L15 - Event Handing Part 3/index.js
@@ -5,16 +5,18 @@ form Event:
       // var name                             form -id name
 const feedbackForm = document.getElementById("feedbackForm");
 
-                                          //div -id name
+                                                                   // select two element's 
+       //another id                         //div -id name
 const  responseDiv = document.getElementById("response ");
+
 
 console,log (feedbackForm);
 
-                              //event?              //argument 
+                              //event?             //argument 
 feedbackForm.addEventListener('subnit', ( functin ( event ) =>{      
                                          //anomoys function                  
           //trigger event                                                                
-    event.preventDefault  (); // input ulla press pannA PANNA TIGGER aagum
+ event.preventDefault  (); // input ulla press pannA PANNA TIGGER aagum
 
   feedbackForm.addEventListner("submit",  function ( event)){
 
@@ -22,23 +24,23 @@ feedbackForm.addEventListener('subnit', ( functin ( event ) =>{
 
     const formData = new FormData(this);
 
-    console.log('formDats');  
+    console.log('formDats');    // ithu vara key value pair aa kaatum
 
-    const formDataJson ={};
+    const formDataJson ={};         //json format converting
 
     formData.forEach((value,key ) => {
 
-          formDataJson[key ] = value; 
+          formDataJson[ key ] = value;   // object formatt aa show aagum.
   
 });
 
     console.log('formDataJson ');  
 
-    const jsonString = JSON.stringif ( formDataJson, null ,2);
+    const jsonString = JSON.stringif ( formDataJson, null ,2);  
 
-    responseDiv.innerHTML = "<pre> $ { jsonString  } </pre>";
+    responseDiv.innerHTML = "<pre> $ { jsonString  } </pre>";   // ui laa string aa display aagum
 
-    feedbackForm.rest();
+    feedbackForm.rest();   // rest event
 
   });
                                
