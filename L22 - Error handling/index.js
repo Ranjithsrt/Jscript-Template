7c5ea8588ec46cdf2b9a81152b9581d5c1ead7d5
Ranjithsrt <ranjith201099@gmail.com>
2024-09-06 21:55:41 +0530

    js-Error Handling

---
+++ L22 - Error handling/index.js
@@ -0,0 +1,19 @@
+Error handling
+
+
+Error handling in JavaScript is a way to manage and respond to errors that
+occur during the execution of your code. It helps ensure that your program
+can handle unexpected situations gracefully and provide useful feedback
+to users or developers.
+  
+Syntax
+
+Syntax
+
+
+try
+// Code Block
+}catch(error){
+// Code Block
+}finally{
+// Code that runs no matter what
